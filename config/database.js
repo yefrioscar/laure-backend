f353645caa0a2543f90894c6182c0a07111c9afb
Yefri Laura <yefrioscar9814@gmail.com>
2020-11-18 23:46:36 -0500

    add

--- config/database.js
+++ config/database.js
@@ -4,23 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', '')
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: false,
-        pool: {
-          min: 0,
-          max: 50,
-          idleTimeoutMillis: 50000,
-          createTimeoutMillis: 50000,
-          acquireTimeoutMillis: 50000
-        }
-      }
-    }
-  }
-})
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
